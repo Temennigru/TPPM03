9899b672d903fd5573a858c9d66f113e7a8992ba
joaopenna <joaopenna@gmail.com>
2014-10-02 14:32:24 -0300

    Update Console.java

--- Console.java
+++ Console.java
@@ -8,7 +8,26 @@ public class Console{
     //gameCore
     //help
     //
-
-
+    
+    //interface:
+    
+    //numero de cartas (do adversario principalmente)
+    //mana pool
+    //suas cartas
+    //cemiterio
+    //mesa
+    
+    //acoes:
+    
+    //virar uma carta
+    //      -> gera efeito (adiciona mana ao mana pool ou ativa uma abilidade)
+    //jogar uma carta da sua mao
+    //      -> selecionar de onde vai vir a mana (virar cartas de terreno ou ativar da mana pool)
+    //      -> fast effects
+    //skip para a proxima fase do turno
+    //      ->
+    //descricao de uma carta
+    //      -> le a descricao da carta, power/toughness, custo de mana
+    
 
 }
