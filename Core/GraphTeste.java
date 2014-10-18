e135842fb33555de07ebf134c4c766aef2b59afd
joaopenna <joaopenna@gmail.com>
2014-10-18 20:06:38 -0300

    Create GraphTeste.java

---
+++ Core/GraphTeste.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class GraphTeste {
+  //teste para a interface grafica
+
+	//public static void main (String[] args){
+	//	JFrame frame = new GraphTable ("Magic TP2 Table");
+	//}
+}	
