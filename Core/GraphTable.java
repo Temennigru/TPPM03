d308f90e6cb22dd5d0fc248e82b374c07e5f0760
joaopenna <joaopenna@gmail.com>
2014-10-18 20:04:48 -0300

    Create GraphTable.java

---
+++ Core/GraphTable.java
@@ -0,0 +1,18 @@
+import java.awt.BorderLayout;
+import java.awt.Container;
+import javax.swing.JFrame;
+
+public class GraphTable extends JFrame{
+	public GraphTable (String title){
+		setSize (400,300);
+		setTitle(title);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Container contentPane = getContentPane();
+		contentPane.setLayout (new BorderLayout());
+		contentPane.add(new GraphHand(),"South");
+		contentPane.add(new GraphAIHand(),"North");
+		setVisible(true);
+	
+	}
+}
