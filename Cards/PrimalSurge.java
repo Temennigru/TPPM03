85a1c93acddd8bec2eb3aa64b97b2d9d27095f5b
joaopenna <joaopenna@gmail.com>
2014-10-19 20:11:38 -0200

    Create PrimalSurge.java

---
+++ Cards/PrimalSurge.java
@@ -0,0 +1,5 @@
+//reference: http://magiccards.info/avr/en/189.html
+//8GG
+//sorcery
+//Exile the top card of your library. If it's a permanent card, you may put it onto the battlefield. If you do, repeat this process.
+//With Avacyn's return, the flow of life became a tidal wave.
