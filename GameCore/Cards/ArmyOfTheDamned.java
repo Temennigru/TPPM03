b01a17f8a38739da7671cbd90509fe1e56a5c023
joaopenna <joaopenna@gmail.com>
2014-10-20 06:52:22 -0200

    Update ArmyOfTheDamned.java

--- GameCore/Cards/ArmyOfTheDamned.java
+++ GameCore/Cards/ArmyOfTheDamned.java
@@ -2,8 +2,8 @@ package GameCore.Cards;
 
 //reference: http://magiccards.info/c13/en/69.html
 
-// UUUUUKKK
+// 5BBB
 // sorcery
 // Put thirteen 2/2 black Zombie creature tokens onto the battlefield tapped.
-// Flashback UUUUUUUKKK (You may cast this card from your graveyard for its flashback cost. Then exile it.)
+// Flashback 7BBB (You may cast this card from your graveyard for its flashback cost. Then exile it.)
 // flavor: "Sometimes death comes knocking. Sometimes it tears down the walls."
