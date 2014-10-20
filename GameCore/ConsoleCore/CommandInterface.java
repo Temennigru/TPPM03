e29ec613b01bd00d7ba4cebb4110376d6350907e
Temennigru <temennigru@github.com>
2014-10-20 01:45:21 -0200

    Adding console interface for easier access

--- GameCore/ConsoleCore/CommandInterface.java
+++ GameCore/ConsoleCore/CommandInterface.java
@@ -1,11 +1,11 @@
 package GameCore.ConsoleCore;
 
-public class Command {
-    private Command() {}
-
+public interface CommandInterface {
     public abstract String getName();
 
     public abstract String getDescription();
 
     public abstract void Exec (String[] args);
+
+	public abstract void setOutput(String output);
 }
