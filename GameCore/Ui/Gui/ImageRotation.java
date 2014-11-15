90a8046fc8350f715a164fae525f1edb6dcfce96
joaopenna <joaopenna@gmail.com>
2014-11-15 00:26:42 -0200

    Create ImageRotation.java

---
+++ GameCore/Ui/Gui/ImageRotation.java
@@ -0,0 +1,34 @@
+package GameCore.Ui.Gui;
+import GameCore.GameObjectCore.*;
+import java.awt.*;
+import javax.swing.*;
+import java.io.File;
+import javax.imageio.ImageIO;
+
+public class ImageRotation {
+    
+    //test
+    public static void main(String args[]) throws Exception {
+        JFrame frame = new JFrame("Rotation Test");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        final BufferedImage bi = ImageIO.read(new File("C:\\Users\\40\\Desktop\\107.jpg"));
+        frame.add(new JPanel() {
+
+            @Override
+            public Dimension getPreferredSize() {
+                return new Dimension(bi.getWidth(), bi.getHeight());
+            }
+
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Graphics2D g2 = (Graphics2D) g;
+                g2.rotate(Math.PI / 2, 0, bi.getHeight() / 1 );
+                g2.drawImage(bi, 0, 0, null);
+            }
+        });
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
