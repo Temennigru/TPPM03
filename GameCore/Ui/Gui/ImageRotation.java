d1653db6bde4d721570728e1cd857052668a0c61
joaopenna <joaopenna@gmail.com>
2014-11-18 18:26:01 -0200

    Update ImageRotation.java

--- GameCore/Ui/Gui/ImageRotation.java
+++ GameCore/Ui/Gui/ImageRotation.java
@@ -6,21 +6,29 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
+
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class ImageRotation {
-    
-    //test
+	
     public static void main(String args[]) throws Exception {
         JFrame frame = new JFrame("Rotation Test");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         final BufferedImage bi = ImageIO.read(new File("C:\\Users\\40\\Desktop\\107.jpg"));
+        
+        
         frame.add(new JPanel() {
 
-            @Override
+            /**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+
+			@Override
             public Dimension getPreferredSize() {
                 return new Dimension(bi.getWidth(), bi.getHeight());
             }
@@ -29,8 +37,21 @@ public class ImageRotation {
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
                 Graphics2D g2 = (Graphics2D) g;
-                g2.rotate(Math.PI / 2, 0, bi.getHeight() / 1 );
+                
+                double sin = Math.abs(Math.sin(Math.toRadians(90))),
+                        cos = Math.abs(Math.cos(Math.toRadians(90)));
+
+                int w = bi.getWidth(null), h = bi.getHeight(null);
+
+                int neww = (int) Math.floor(w*cos + h*sin),
+                     newh = (int) Math.floor(h*cos + w*sin);
+                
+                g2.translate((neww-w)/2, (newh-h)/2);
+                g2.rotate(Math.toRadians(90), bi.getWidth()/2, bi.getHeight()/2);
+//              g2.rotate(Math.PI / 4, bi.getWidth() / 2, bi.getHeight() / 2 );
                 g2.drawImage(bi, 0, 0, null);
+                
+                
             }
         });
         frame.pack();
