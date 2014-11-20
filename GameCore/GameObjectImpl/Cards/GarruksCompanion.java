e9cbbbb0695b89d55b70eb87410b3913ce2ce53d
Temennigru <temennigru@github.com>
2014-11-19 22:12:05 -0200

    Adding example of new card implementation to Garruk's companion

--- GameCore/GameObjectImpl/Cards/GarruksCompanion.java
+++ GameCore/GameObjectImpl/Cards/GarruksCompanion.java
@@ -1,47 +1,53 @@
 //reference: http://magiccards.info/m12/en/175.html
 
-package GameCore.Cards;
+package GameCore.GameObjectImpl.Cards;
 
 import GameCore.*;
 import GameCore.GameObjectCore.*;
 import java.lang.System;
 
-public class GarruksCompanion extends Creature {
+public static class GarruksCompanion {
 
-	public GarruksCompanion () {
-        this.trample = true;
-        this.reset();
-        this.m_imgLocation = "GameCore/Cards/Img/GarruksCompanion.jpg";
-        this.m_type = new GameEnums.Type[1];
-        this.m_type[0] = GameEnums.Type.CREATURE;
-        this.m_sub = new GameEnums.CreatureSubType[1];
-        this.m_sub[0] = GameEnums.CreatureSubType.BEAST;
-        this.manaCost = "GG";
-        this.name = "Garruk's Companion";
-        this.description = "Trample"
-        + "(If this creature would assign enough damage to its blockers to destroy them, you may have it assign the rest of its damage to defending player or planeswalker.)";
-        this.flavor = "They seek out living trees to glean age-old secrets from sap and wood.";
-    }
+    private GarruksCompanion(){};
 
-    private void reset() {
-        this.power = 3;
-        this.toughness = 2;
-        this.untap();
-        this.damage = 0;
-    }
+    public Card getInstance(Player owner) {
 
-    public void play () throws GameExceptions.GameException {
-        this.place (GameEnums.Zone.BATTLEFIELD);
-    }
+        GameEnums.SuperType[] supertypes = { };
+        GameEnums.Type[] types = { GameEnums.CREATURE };
+        GameEnums.SubType[] subtypes = { GameEnums.BEAST };
 
-    public void place (GameEnums.Zone zone, int position) throws GameExceptions.GameException {
-        this.reset();
-        this.location = zone;
-        GameCore game = GameCore.getGame();
-        game.registerOnZone(this, zone);
-    }
+        Card ret = new GameCore.GameObjectImpl.CardImpl(
+            "Garruk's Companion",
+            "GG",
+            supertypes,
+            types,
+            subtypes,
 
-    public void kill () throws GameExceptions.GameException {
-        this.place (GameEnums.Zone.GRAVEYARD);
+
+            false, // deathtouch
+            false, // defender
+            false, // doublestrike
+            false, // firststrike
+            false, // flash
+            false, // flying
+            false, // haste
+            false, // hexproof
+            false, // indestructible
+            false, // intimidate
+            "",    // landwalk
+            false, // lifelink
+            "",    // protection
+            false, // reach
+            true,  // trample
+            false, // vigilance
+            0,     // regen
+            false, // abilities
+            3,     // power
+            2,     // toughness
+            "They seek out living trees to glean age-old secrets from sap and wood.", // flavor
+            false, // stackable
+            owner, // owner
+            );
+        return ret;
     }
 }
